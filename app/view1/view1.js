8751bdf135e981ba45252f31da3108eda08ec2c0
Marek Bućko <buckomarek1@gmail.com>
2018-06-24 12:40:32 +0200

    hfidosugiofgaihgrewiagfir

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,42 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', ['$http', function ($http) {
+        var self = this;
+        this.lista = [];
+        this.zmiennaZPolaTekstowego = '';
 
-}]);
+        this.daneZSerwera = {};
+        this.dodajDoListy = function () {
+            self.lista.push({
+                'name': self.nazwa,
+                'quantity': self.ilosc
+            });
+
+            console.log('Dodaje:  ' + self.lista);
+
+        };
+        this.funkcja1 = function () {
+
+            console.log("klik");
+            for (var index in self.lista) {
+                var elementDoWyslania = self.lista[index];
+                $http.post('http://localhost:8080/dostawa', elementDoWyslania)
+                    .then(function (dane) {
+                            console.log('Udalo sie');
+                            self.daneZSerwera = dane;
+                        },
+                        function (error) {
+                            console.log('Error');
+                        });
+            }
+
+        }
+    }]);
